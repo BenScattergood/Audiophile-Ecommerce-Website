9508c916027cda9d54606d2329bd9b1b84ed64f9
Ben Scattergood <ben.s1234@hotmail.co.uk>
2022-11-05 15:22:59 +0000

    home screen styling complete

--- AudiophileEcommerceWebsite/wwwroot/js/site.js
+++ AudiophileEcommerceWebsite/wwwroot/js/site.js
@@ -3,9 +3,11 @@ const app = {
     hamburger: document.querySelector(".hamburger"),
     navFilter: document.querySelector(".nav__filter"),
     navMenu: document.querySelector(".navbar__menu"),
+    body: document.querySelector(".body"),
 }
 
 app.hamburger.addEventListener("click", function() {
     app.navFilter.classList.toggle("hidden");
     app.navMenu.classList.toggle("hidden");
+    app.body.classList.toggle("overflow-y-hidden");
 })
